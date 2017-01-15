28e35b92c2146ae72863ad39bcd6a3aaf34d3729
George Graves <gtgraves@gmail.com>
2017-01-15 17:04:11 -0600

    Assignment 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,16 @@
-var animatePoints = function() {
+var animatePoints = function () {
                 
     var points = document.getElementsByClassName('point');
-                
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-                
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-                
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-                
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    
+    function revealPoint(className) {
+        className[i].style.opacity = 1;
+        className[i].style.transform = "scaleX(1) translateY(0)";
+        className[i].style.msTransform = "scaleX(1) translateY(0)";
+        className[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(points);
+    }
                 
 };
