fc416d13037d4c876a5f89a4abb89c596d7f5a5d
George Graves <gtgraves@gmail.com>
2017-02-18 17:13:29 -0600

    Checkpoint 17 completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    var template =
     '<div class="collection-album-container column fourth">'
 +   '   <img src="assets/images/album_covers/01.png"/>'
 +   '   <div class="collection-album-info caption">'
@@ -12,13 +13,17 @@ var collectionItemTemplate =
 +   '       </p>'
 +   '   </div>'
 +   '</div>'
-;
+    ;
+    
+    return $(template);
+};
 
-window.onload = function () {
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    collectionContainer.innerHTML = '';
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
     
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
